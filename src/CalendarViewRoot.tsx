d7d95b28ac28427b1d66ca058f446a9f10589ced
Daiki Hashimoto <daisea3e1203@gmail.com>
2025-09-19 00:40:16 +0900

    ✨ try out tailwind

--- src/CalendarViewRoot.tsx
+++ src/CalendarViewRoot.tsx
@@ -4,5 +4,10 @@ import { useApp } from "./hooks/useApp";
 export const CalendarViewRoot = () => {
 	const { vault } = useApp();
 
-	return <h4>Hello {vault?.getName()}!</h4>;
+	return (
+		<div className="p-4">
+			<h4>Hello {vault?.getName()}!</h4>
+			<div className="border rounded-md p-2">yes</div>
+		</div>
+	);
 };
