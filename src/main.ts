23d173e5d4bdd2eb5ffb776102d3fb45ad58bae3
Daiki Hashimoto <daisea3e1203@gmail.com>
2025-09-27 16:10:38 +0900

    🗑️ clean example code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import {
 	App,
-	Editor,
-	MarkdownView,
-	Modal,
 	Plugin,
 	PluginSettingTab,
 	Setting,
@@ -33,59 +30,8 @@ export default class DabinCalendarPlugin extends Plugin {
 		// const statusBarItemEl = this.addStatusBarItem();
 		// statusBarItemEl.setText("Status Bar Text");
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "dabin-calendar-start",
-			name: "Add scheduled metadata to list item",
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				const today = new Date();
-				const formattedDate = today.toISOString().split("T")[0];
-				editor.replaceSelection(`[scheduled::${formattedDate}]`);
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-		// 	console.log("click", evt);
-		// });
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
 
 		/**
 		 * Register calendar view
@@ -143,22 +89,6 @@ export default class DabinCalendarPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
 
